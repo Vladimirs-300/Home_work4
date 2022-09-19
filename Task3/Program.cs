00dc9b643a295e8f9b21082c899b91a97a2e8103
vladimir <slr-klesks-300@yandex.ru>
2022-09-20 00:18:09 +1000

    Массив из любого количества элементов

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+int[] Create(int lenght)
+{
+    int[] array = new int[lenght];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(10, 100);
+    }
+    return array;
+}
+void DisplayArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+}
+DisplayArray(Create(8));
